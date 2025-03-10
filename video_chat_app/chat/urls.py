91b9fa2dc3b9c04da8927ecf608e8a854540fd41
mdrayhan03 <phylosopherhossain2022@gmail.com>
2025-03-11 05:46:22 +0600

    commit

--- video_chat_app/chat/urls.py
+++ video_chat_app/chat/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import room
+from . import views
 
 app_name = "chat"
 
 urlpatterns = [
-    path('chat/<str:room_name>/', room, name='room'),
+    path('chat/<str:room_name>/', views.room, name='room'),
+    path("", views.login, name="login"),
+    path("registration/", views.registration, name="registration"),
+    path("password/", views.password, name="password"),
+    path("dashboard/", views.dashboard, name="dashboard"),
 ]

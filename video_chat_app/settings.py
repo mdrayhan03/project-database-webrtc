6455420454084d642c6379cbe6d3f620c02b7873
mdrayhan03 <phylosopherhossain2022@gmail.com>
2025-03-12 02:45:01 +0600

    commit

--- video_chat_app/settings.py
+++ video_chat_app/settings.py
@@ -84,7 +84,12 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
